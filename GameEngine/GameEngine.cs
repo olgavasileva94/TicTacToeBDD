0ae80684a5bc03a4bbb4d9ad72e23285a7143a92
Ольга Васильева <stereomatrass@mail.ru>
2018-05-26 17:14:29 +0700

    Настройка пакетов

---
+++ GameEngine/GameEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class GameEngine
+    {
+        public void CreatePlayer(int id, String name)
+        {
+
+        }
+
+        public void GetPlayer(int id)
+        {
+
+        }
+
+        public void DeletePlayer(int id)
+        {
+
+        }
+
+        public int GetNumberOfPlayers()
+        {
+            return 0;
+        }
+    }
+}
