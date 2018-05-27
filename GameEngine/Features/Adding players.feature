59b78f7291364dac351e25e47f701417fab7e07b
Ольга Васильева <stereomatrass@mail.ru>
2018-05-27 11:26:29 +0700

    Реализовано: Feature: Adding players. Scenario: Add 1 player, Scenario: Add 2 player, Scenario: Add 3 player. Методы-заглушки: AddPlayer, GetNumberOfPlayers. Сценарии не проходят: Add 1 player, Add 2 player, Add 3 player.

---
+++ GameEngine/Features/Adding players.feature
@@ -0,0 +1,25 @@
+﻿Feature: Adding players
+	To add a player
+	As a game engine
+	I want to create a player and add to the game engine
+
+@add 1 player
+Scenario: Add 1 player
+	Given I have a game engine
+	And No players in game engine
+	When I add 1 player to game engine
+	Then Game engine should contains 1 player
+
+@add 2 player
+Scenario: Add 2 player
+	Given I have a game engine
+	And No players in game engine
+	When I add 2 player to game engine
+	Then Game engine should contains 2 player
+
+@add 3 player
+Scenario: Add 3 player
+	Given I have a game engine
+	And No players in game engine
+	When I add 3 player to game engine
+	Then Game engine should contains 2 player
---
+++ GameEngine/Features/Steps/AddingPlayersSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace GameEngine.Features.Steps
+{
+    [Binding]
+    public class AddingPlayersSteps
+    {
+        GameEngine gameEngine;
+
+        [Given(@"I have a game engine")]
+        public void GivenIHaveAGameEngine()
+        {
+            this.gameEngine = new GameEngine();
+        }
+        
+        [Given(@"No players in game engine")]
+        public void GivenNoPlayersInGameEngine()
+        {
+            Assert.AreEqual(this.gameEngine.GetNumberOfPlayers(), 0);
+        }
+        
+        [When(@"I add (.*) player to game engine")]
+        public void WhenIAddPlayerToGameEngine(int p0)
+        {
+            for (int i = 0; i < p0; i ++)
+            {
+                if (i == 2)
+                {
+                    Assert.AreEqual(this.gameEngine.AddPlayer(), false);
+                } else
+                {
+                    Assert.AreEqual(this.gameEngine.AddPlayer(), true);
+                }
+            }
+        }
+        
+        [Then(@"Game engine should contains (.*) player")]
+        public void ThenGameEngineShouldContainsPlayer(int p0)
+        {
+            Assert.AreEqual(this.gameEngine.GetNumberOfPlayers(), p0);
+        }
+    }
+}
